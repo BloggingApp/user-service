fa9fd4fe396cd82013cc449b2529120b6b8cd383
morf1lo <abdullinti@gmail.com>
2025-04-01 19:13:15 +0500

    publish new post notifications enabled update to rabbitmq

--- internal/rabbitmq/queues.go
+++ internal/rabbitmq/queues.go
@@ -4,4 +4,5 @@ const (
 	REGISTRATION_CODE_MAIL_QUEUE = "notifications.registration_code"
 	SIGNIN_CODE_MAIL_QUEUE = "notifications.signin_code"
 	FOLLOWS_QUEUE = "follows"
+	FOLLOWERS_NEW_POST_NOTIFICATIONS_ENABLED_UPDATES_QUEUE = "followers-new-post-notifications-enabled-updates"
 )
